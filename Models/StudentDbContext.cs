11afdd6ef99c2a11f55546ba1f37ca1ae22f5b9f
Lasitha Heenkenda <Lasithaheenkenda23@gmail.com>
2023-07-11 19:01:25 +0530

    backend updated

---
+++ Models/StudentDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace React_with_Asp.Net_Core_web_Api_Full_Stack_Crud_Application.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer("Data Source=.; initial Catalog=lbs; User Id=sa; password=123; TrustServerCertificate=True");
+        }
+
+    }
+}
