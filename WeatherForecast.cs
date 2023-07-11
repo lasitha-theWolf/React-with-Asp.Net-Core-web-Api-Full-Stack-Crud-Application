9ad725d4f0ceb78622573824b3169cac2167a096
Lasitha Heenkenda <Lasithaheenkenda23@gmail.com>
2023-07-11 16:35:31 +0530

    Initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace React_with_Asp.Net_Core_web_Api_Full_Stack_Crud_Application
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
